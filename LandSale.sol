16dd810a261a2d935d339f4c3196a8724b498faa
nikofebrianur <nikofebrianur@gmail.com>
2023-05-09 20:48:16 +0800

    add BidDetail struct

---
+++ LandSale.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2 <0.9.0;
+
+import "./Land.sol";
+
+contract LandSale is LandRegistration {
+
+    struct BidDetail {
+        uint256 highestBid;
+        address highestBidder;
+        mapping(address => uint256) bids;
+        address[] bidders;
+    }
+}
