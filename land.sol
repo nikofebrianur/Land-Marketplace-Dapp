3f3c28b36711504ac7741083ac21f6f8530742ec
nikofebrianur <nikofebrianur@gmail.com>
2023-05-09 20:44:27 +0800

    change the contract name

--- land.sol
+++ land.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.2 <0.9.0;
 
-contract LandMarkeplaceDapp {
+contract LandRegistration  {
     struct LandDetail {
         string province;
         string district;
