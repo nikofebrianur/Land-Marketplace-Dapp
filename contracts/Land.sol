7c9eebbcfd2c87deae4043dce7f181c89367288e
nikofebrianur <nikofebrianur@gmail.com>
2023-05-10 20:46:47 +0800

    import openzeppelin ERC721 module

--- contracts/Land.sol
+++ contracts/Land.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.2 <0.9.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; 
+
 contract LandRegistration  {
     struct LandDetail {
         string province;
