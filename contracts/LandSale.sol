55eccf89675c4126fbfd57b195ac6957615deeb4
nikofebrianur <nikofebrianur@gmail.com>
2023-05-16 10:00:44 +0800

    add bid event

--- contracts/LandSale.sol
+++ contracts/LandSale.sol
@@ -4,7 +4,6 @@ pragma solidity >=0.8.2 <0.9.0;
 import "./Land.sol";
 
 contract LandSale is LandRegistration {
-
     struct BidDetail {
         uint256 highestBid;
         address highestBidder;
@@ -15,21 +14,37 @@ contract LandSale is LandRegistration {
 
     function bid(uint256 landID) public payable {
         require(msg.sender != ownerOf(landID));
-        require(bidDetails[landID].bids[msg.sender] + msg.value > bidDetails[landID].highestBid, "can't bid, make a higher Bid");
+        require(
+            bidDetails[landID].bids[msg.sender] + msg.value >
+                bidDetails[landID].highestBid,
+            "can't bid, make a higher Bid"
+        );
         bidDetails[landID].highestBidder = msg.sender;
         bidDetails[landID].bidders.push(msg.sender);
-        bidDetails[landID].highestBid = bidDetails[landID].bids[msg.sender] + msg.value;
+        bidDetails[landID].highestBid =
+            bidDetails[landID].bids[msg.sender] +
+            msg.value;
         bidDetails[landID].bids[msg.sender] += msg.value;
+        emit BidEvent(
+            landID,
+            bidDetails[landID].highestBidder,
+            bidDetails[landID].highestBid
+        );
     }
 
     function acceptBid(uint256 landID) public {
         payable(msg.sender).transfer(bidDetails[landID].highestBid);
         safeTransferFrom(msg.sender, bidDetails[landID].highestBidder, landID);
-        for(uint256 i = 0; i < bidDetails[landID].bidders.length; i++) {
-            if (bidDetails[landID].bidders[i] == bidDetails[landID].highestBidder) {
+        for (uint256 i = 0; i < bidDetails[landID].bidders.length; i++) {
+            if (
+                bidDetails[landID].bidders[i] ==
+                bidDetails[landID].highestBidder
+            ) {
                 continue;
             }
-            payable(bidDetails[landID].bidders[i]).transfer(bidDetails[landID].bids[bidDetails[landID].bidders[i]]);
+            payable(bidDetails[landID].bidders[i]).transfer(
+                bidDetails[landID].bids[bidDetails[landID].bidders[i]]
+            );
         }
         landDetails[landID].salesStatus = true;
     }
@@ -37,4 +52,10 @@ contract LandSale is LandRegistration {
     fallback() external {
         revert();
     }
+
+    event BidEvent(
+        uint256 indexed landID,
+        address highestBidder,
+        uint256 highestBid
+    );
 }
